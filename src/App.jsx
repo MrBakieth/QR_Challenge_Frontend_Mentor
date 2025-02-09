902c3a611dae40a810d8b90198436a74972e65ec
H.Furkan Yaman <ouro_30@outlook.com>
2025-02-09 16:35:32 +0300

    Fixed

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,11 @@ const App = () => {
   return (
     <div className='container'>
       <div className='card'>
-        <img src="/anonymous_qr_code.png" alt="QR code" className='qr-code' />
+        <img 
+          src="/public/anonymous_qr_code.png"
+          alt="QR code" 
+          className='qr-code' 
+        />
         <h3 className='title-text'>Improve your front-<br/>end skills by building projects</h3>
         <p className='description-text'>Scan the QR code to visit Frontend <br/> Mentor and take your coding skills to <br/> the next level</p>
       </div>
