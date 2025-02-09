fb7466d7894dcf8ecffeb76a68e0097d6b830121
H.Furkan Yaman <ouro_30@outlook.com>
2025-02-09 16:29:19 +0300

    Fixed:QR code image import path.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import qrCode from '/assets/anonymous_qr_code.png'
+import qrImage from './assets/anonymous_qr_code.png'
 
 const App = () => {
   return (
     <div className='container'>
       <div className='card'>
-        <img src={qrCode} alt="QR Code" className='qr-code' />
+        <img src={qrImage} alt="QR code" className='qr-code' />
         <h3 className='title-text'>Improve your front-<br/>end skills by building projects</h3>
         <p className='description-text'>Scan the QR code to visit Frontend <br/> Mentor and take your coding skills to <br/> the next level</p>
       </div>
